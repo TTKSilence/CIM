17b3975be49dba30b6498e6e7d358d4b3a538d9f
TTKSilence <wangpc22kk@gmail.com>
2019-11-21 20:19:17 +0800

    commit to 'master'

--- cim_mainpage.py
+++ cim_mainpage.py
@@ -7,7 +7,7 @@ from tkinter import ttk
 from tkinter import messagebox
 import numpy
 #import database
-import popup_withouthistory
+#import popup_withouthistory
 import pymysql
 
 
@@ -22,9 +22,9 @@ class Application(ttk.Frame):
         self.data()
         #self.Searchachievement()
         self.item
-        self.sitem
-        self.user_input
-        self.search_data()
+        #self.sitem
+        #self.user_input
+        #self.search_data()
         
 
 
@@ -54,11 +54,82 @@ class Application(ttk.Frame):
         #return user_input
         
 
+    def createWidgets(self):
+
+
+        def search_data():
+            #连接customer data 数据库
+            cd=pymysql.connect(host="127.0.0.1",port=3306,user="CIM",password="pingan1234",db="customerdata") #,charset="utf-8"
+            #创建游标
+            cursor=cd.cursor()
+            #查询语句
+
+            #将输入框中产生的数据导出
+            #选定的输入项目
+            sitem=self.item.get()
+            if sitem=='车牌号码':
+                sitem='carnumber'
+            elif sitem=='客户姓名':
+                sitem='name'
+            else:
+                sitem='tel'
+        
+            #self.sitem=sitem
+
+
+            #输入内容
+            user_input=self.search_entry.get()
+
+
+        
+            sit=sitem
+            sip=user_input
+            print(sit)
+            print(sip)
+    
+            sql="SELECT * FROM EINS WHERE %s ='%s'" %(sit,sip)
+    
+            try:
+                #执行SQL语句
+                cursor.execute(sql)
+                #获取所有符合条件的搜索记录
+                results=cursor.fetchall()
+                #return results
+        
+                for row in results:
+                    carnumber=row[0]
+                    name=row[1]
+                    tel=row[2]
+                    time=row[3]
+                    item=row[4]
+                    cost=row[5]
+                    note=row[6]
+                    #打印结果
+                    print("carnumber=%s,name=%s,tel=%s,time=%s,item=%s,cost=%s,note=%s"% \
+                        (carnumber,name,tel,time,item,cost,note))
+        
+            except:
+                print( "Error:unable to fetch data!")
+
+            #关闭数据库连接
+            cursor.close()
+            cd.close()
+
+
+        self.searchButton = ttk.Button(self, text='查询',command=search_data)  #主页 查询 接口调用
+        self.searchButton.grid() 
 
 
 
 
 
+        self.addButton=ttk.Button(self,text='添加',command=self.add_record)   #主页 添加 接口调用
+        self.addButton.grid()
+
+
+
+
+    '''
     def search_data(self):
         #连接customer data 数据库
         cd=pymysql.connect(host="127.0.0.1",port=3306,user="CIM",password="pingan1234",db="customerdata") #,charset="utf-8"
@@ -117,7 +188,7 @@ class Application(ttk.Frame):
         cursor.close()
         cd.close()
 
-
+    '''
 
 
 
